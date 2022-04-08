585a214876dd98a5fc2420f3e708fb60e0447f77
nguyenminhvu4901 <nguyenminhvu4901@gmail.com>
2022-04-08 23:33:13 +0700

    Update gradle.xml

---
+++ app/src/main/java/view/PhoneList.java
@@ -0,0 +1,16 @@
+package view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.btl.R;
+
+public class PhoneList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_list);
+    }
+}
